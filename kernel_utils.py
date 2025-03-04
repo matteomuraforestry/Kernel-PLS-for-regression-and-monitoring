b73d03e8298d2775640914032e800951794b2297
Matteo <151618958+matteomuraforestry@users.noreply.github.com>
2025-03-04 12:06:18 +0100

    Update kernel_utils.py with the function rbf_kernel
    Added the function rbf_kernel (as defined in https://github.com/dnackat/mitx-6.86x-machine-learning/blob/master/project2/mnist/part1/kernel.py in order to use PLSR for non linear modeling.

--- kernel_utils.py
+++ kernel_utils.py
@@ -6,6 +6,29 @@ Utility functions used to generate Kernel matrices for training and test dataset
 
 import numpy as np
 
+def rbf_kernel(X, Y, gamma):
+    """
+    Compute the Gaussian RBF kernel between two matrices X and Y::
+        K(x, y) = exp(-gamma ||x-y||^2)
+    for each pair of rows x in X and y in Y.
+
+    Args:
+        X - (n, d) NumPy array (n datapoints each with d features)
+        Y - (m, d) NumPy array (m datapoints each with d features)
+        gamma - the gamma parameter of gaussian function (scalar)
+
+    Returns:
+        kernel_matrix - (n, m) Numpy array containing the kernel matrix
+    """
+    n, d = X.shape
+    m = Y.shape[0]
+
+    kernel_matrix = X**2 @ np.ones((d,m)) + np.ones((n,d)) @ Y.T**2 - 2*(X @ Y.T)
+    kernel_matrix = np.exp(-gamma*kernel_matrix)
+    
+    return kernel_matrix
+
+
 def Kernel(X, width):
     """
 
